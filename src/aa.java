89208a929aa3e0639b648005d7aeca51a9d5169b
xiaowq <18210088850@163.com>
2021-02-03 18:03:00 +0800

    first blood

--- src/aa.java
+++
@@ -1,9 +0,0 @@
-public class aa {
-    static void  recursionBinarySearch(int array[], int key, int low, int high) {
-        System.out.println("niubility ");
-        System.out.println("niubility ");
-        System.out.println("niubility ");
-        System.out.println("niubility ");
-        System.out.println("niubility ");
-    }
-}
--- src/com/company/james/Main.java
+++ src/com/company/james/Main.java
@@ -1,16 +1,8 @@
 package com.company.james;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        show();
+
     }
-
-    private static void show(String... list) {
-        System.out.println(Arrays.toString(list));
-    }
-
-
 }
