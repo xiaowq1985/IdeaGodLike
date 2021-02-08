6881bae1cd5f849247089253874fe334dcf82cb3
xiaowq <18210088850@163.com>
2021-02-08 16:53:57 +0800

    冒泡排序

--- src/com/company/james/arithmetic/SelectSort.java
+++ src/com/company/james/arithmetic/SelectSort.java
@@ -7,8 +7,7 @@ public class SelectSort extends Sort {
     }
 
     public void sort() {
-        System.out.println("Origin");
-        display();
+
         for (int i = 0; i < array.length - 1; i++) {
             int minPosition = i;
             for (int j = i + 1; j < array.length; j++) {
@@ -17,10 +16,7 @@ public class SelectSort extends Sort {
                 }
             }
             swap(i, minPosition);
-            System.out.println("第" + i + "次循环");
-            display();
         }
-        System.out.println("Finally");
-        display();
+
     }
 }
